d1f9535decc78d1ed1869720f48e58d0af6be34a
srafi1 <shakilrafi0@gmail.com>
2017-12-19 11:01:28 -0500

    working

--- forking_server.c
+++ forking_server.c
@@ -12,10 +12,31 @@ static void sighandler(int signo) {
 }
 
 int main() {
+    while (1) {
+        int well_known = server_setup();
+        if (!fork()) {
+            subserver(well_known);
+        }
+    }
 }
 
 void subserver(int from_client) {
+    int to_client = server_connect(from_client);
+    while (1) {
+        char buffer[HANDSHAKE_BUFFER_SIZE];
+        read(from_client, buffer, HANDSHAKE_BUFFER_SIZE);
+        printf("Received: [%s]\n", buffer);
+        process(buffer);
+        write(to_client, buffer, HANDSHAKE_BUFFER_SIZE);
+        printf("Sent\n");
+    }
 }
 
 void process(char * s) {
+    int i;
+    for (i = 0; i < strlen(s); i++) {
+        if (s[i] != 0) {
+            s[i] = toupper(s[i]);
+        }
+    }
 }
--- pipe_networking.h
+++ pipe_networking.h
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
 
 #ifndef NETWORKING_H
